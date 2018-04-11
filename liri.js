146a72fa060663c8b62afe0f0a90ed911bf09410
Madeline Wood <maddiewood42@gmail.com>
2018-04-10 21:35:26 -0400

    tweaks

--- liri.js
+++ liri.js
@@ -1,12 +1,13 @@
+
 require("dotenv").config();
 var request = require("request");
-
+var fs = require("fs");
 var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require("node-spotify-api");
-var request = require("request");
 var oper = (process.argv[2]);
 var search = JSON.stringify(process.argv[3]);
+
 
 var getTweets = function () {
     var client = new Twitter(keys.twitter);
@@ -29,6 +30,7 @@ var getTweets = function () {
     });
 }
 
+
 var getTunes = function () {
     var spotify = new Spotify(keys.spotify);
     spotify.search({
@@ -38,9 +40,10 @@ var getTunes = function () {
     }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
-        } else for (var i = 0; i < spotify.search.length; i++) {
-        console.log(JSON.stringify(data.tracks.items[i].album.artists[i].name, null, 4));
-        //console.log(JSON.stringify(data.tracks.items[i].album.artists[i], null, 4));
+        } else {
+                console.log("Artist(s): " + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 4));
+                console.log("Song: " + JSON.stringify(data.tracks.items[0].album.name, null, 4));
+            
         }
     });
 }
@@ -54,32 +57,56 @@ var getMovie = function () {
             console.log("The title is: " + JSON.parse(body).Title);
             console.log("The movie's release date is: " + JSON.parse(body).Year);
             console.log("The IMDB Rating of this movie is: " + JSON.parse(body).imdbRating)
-            console.log("The Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[1].value);
+            console.log("The Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[1].Value);
             console.log("The movie was made in: " + JSON.parse(body).Country);
             console.log("The plot is: " + JSON.parse(body).Plot);
             console.log("The lead actors are: " + JSON.parse(body).Actors);
-        } else if (search === 0) {
-            request("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy"), function(error, response, body) {
+        } else if (response.statusCode != 200) {
+            request("http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy"),
+                function (error, response, body) {
                     console.log("The title is: " + JSON.parse(body).Title);
                     console.log("The movie's release date is: " + JSON.parse(body).Year);
                     console.log("The IMDB Rating of this movie is: " + JSON.parse(body).imdbRating)
-                    console.log("The Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[1].value);
+                    console.log("The Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[1].Value);
                     console.log("The movie was made in: " + JSON.parse(body).Country);
                     console.log("The plot is: " + JSON.parse(body).Plot);
                     console.log("The lead actors are: " + JSON.parse(body).Actors);
-            }
+                }
         }
 
+    });
 }
 
+var doIt = function () {
+
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (error) {
+                return console.log(error);
+            } else {
+                console.log(data);
+                
+            }
+        });
+
+    }
+
+
+  
 if (oper === "my-tweets") {
     getTweets();
 }
 
 if (oper === "spotify-this-song") {
     getTunes();
-}
+} 
 
 if (oper === "movie-this") {
     getMovie();
-}
+} 
+
+if (oper === "do-what-it-says") {
+    doIt();
+} 
+
+
+
